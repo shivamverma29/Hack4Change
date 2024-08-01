6b5506d8abfb04d032c546a9da902277b2ea36e2
Shivam Verma <91418627+shivamverma29@users.noreply.github.com>
2024-08-01 20:46:28 +0530

    legal services

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,25 +12,37 @@ import Crowd from "./components/crowd";
 import Visuals from "./components/VIsuals";
 import Register from "./components/register";
 import Login from "./components/login";
+import { ContextProvider } from "./components/context/StepContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Legal from "./components/legal/legal";
+import Service from "./components/legal/service";
+import InputForm from "./components/legal/inputForm";
 function App() {
   return (
     <>
-      <Nav />
-      <Chatbot />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/lms" element={<Lms />} />
-          <Route path="/uploadvideo" element={<VideoUpload />} />
-          <Route path="/guidex" element={<Guidex />} />
-          <Route path="/poster" element={<Poster />} />
-          <Route path="/crowd" element={<Crowd />} />
-          <Route path="/dashboard" element={<Visuals />} />
-          <Route path="/signup" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer></Footer>
+      <ContextProvider>
+        <Nav />
+        <Chatbot />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lms" element={<Lms />} />
+            <Route path="/uploadvideo" element={<VideoUpload />} />
+            <Route path="/guidex" element={<Guidex />} />
+            <Route path="/poster" element={<Poster />} />
+            <Route path="/crowd" element={<Crowd />} />
+            <Route path="/dashboard" element={<Visuals />} />
+            <Route path="/signup" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/legal" element={<Legal />} />
+            <Route path="/service/:id" element={<Service />} />
+            <Route path="/form/:id" element={<InputForm />} />
+          </Routes>
+          <ToastContainer />
+        </BrowserRouter>
+        <Footer></Footer>
+      </ContextProvider>
     </>
   );
 }
